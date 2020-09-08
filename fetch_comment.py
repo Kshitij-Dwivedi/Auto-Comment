dd1b2c5deef2d975e028c618c9db6e2792befe6f
shantanu <shaantanutripathi@gmail.com>
2020-09-08 16:27:26 +0530

    fetch comment feature

---
+++ fetch_comment.py
@@ -0,0 +1,33 @@
+
+import googleapiclient.discovery
+
+import json 
+
+def pretty_print(data): 
+      
+    data = data
+    print(json.dumps(data, indent = 10) )
+
+"""
+INSERT YOUR OWN API KEY HERE
+"""
+api_key = "AIzaSyBHnforGaIV0YS8w3ZDzWlrHKpi1xE2hTA"
+
+
+
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = api_key
+
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+"""
+INSERT VIDEO ID HERE
+"""
+request = youtube.commentThreads().list(part="snippet,replies",videoId="ZvpdN4VltKY")
+
+response = request.execute()
+
+# print(pretty_print(response["items"][0]))
+for i in range(20):
+    print(i+1,"->",response["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"])
